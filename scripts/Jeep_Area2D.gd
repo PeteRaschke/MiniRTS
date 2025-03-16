90abad8d67052426c49a13fdefda1bfb7059fbf1
Pete <139820333+PeteRaschke@users.noreply.github.com>
2025-03-15 19:25:34 -0700

    Developer art

--- scripts/Jeep_Area2D.gd
+++ scripts/Jeep_Area2D.gd
@@ -4,4 +4,4 @@ func _input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("left_click"):
 		var parent = get_parent()
 		get_node("/root/Map/Player_Interface").add_selected_unit(parent)
-		print("Jeep Selected")
+		print("Tank Selected")
